e41f936105dbae5e05e18b7c44669bcef817d663
BuildTools <unconfigured@null.spigotmc.org>
2023-01-26 22:43:50 -0500

    more functionality and a new page

--- market/__init__.py
+++ market/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = 'd4cea4219d9cd6d62eb68093'
+
 db = SQLAlchemy(app)
 app.app_context().push()
 
